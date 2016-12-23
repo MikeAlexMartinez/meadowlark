cd88dae1053b91c62698ae2306a5034cc9eb7d81
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2016-12-23 21:41:03 +0000

    Added /about tests

--- meadowlark.js
+++ meadowlark.js
@@ -15,6 +15,13 @@ app.set('port', process.env.PORT || 3000);
 // Assign static directory
 app.use(express.static(__dirname + '/public'));
 
+// Page testing
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test ==='1' ;
+	next();
+});
+
 // Routes
 // Homepage
 app.get('/', function(req, res) {
@@ -23,8 +30,20 @@ app.get('/', function(req, res) {
 
 // About
 app.get('/about', function(req, res) {
-	res.render('about', { fortune: fortune.getFortune() });
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
 });
+
+// routes for cross page testing
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
+
 
 // custom 404 page
 app.use(function(req,res) {
