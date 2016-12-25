81903e68810456f1e32ff0cc272b22833f3e2243
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2016-12-25 21:12:47 +0000

    Added weather widget

--- meadowlark.js
+++ meadowlark.js
@@ -22,6 +22,13 @@ app.use(function(req, res, next) {
 	next();
 });
 
+// Weather middleware
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weatherData = getWeatherData();
+	next();
+});
+
 // Routes
 // Homepage
 app.get('/', function(req, res) {
@@ -62,3 +69,32 @@ app.listen(app.get('port'), function() {
 	console.log( 'Express started on http://localhost:' +
 		app.get('port') + '; press Ctrl-C to terminate.' );
 });	
+
+// Mocked weather data
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)'
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)'
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)'
+			}
+		],
+	};
+}
